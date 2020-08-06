11f18fd21103d59edf9dce5f205a969336fcf83f
Prateek Kaushik <pkaran26@Prateek>
2020-08-06 15:36:45 +0530

    minor

--- src/Components/Chat/Message.js
+++ src/Components/Chat/Message.js
@@ -4,13 +4,17 @@ import { Card } from 'primereact/card'
 export const LeftMessage = ({ message, time })=>(
   <Card className="message leftMessage">
     <div>{ message }</div>
-    <small>{ time }</small>
+    <div style={{ textAlign: 'right' }}>
+      <small>{ time }</small>
+    </div>
   </Card>
 )
 
 export const RightMessage = ({ message, time })=>(
   <Card className="message rightMessage">
     <div>{ message }</div>
-    <small>{ time }</small>
+    <div style={{ textAlign: 'right' }}>
+      <small>{ time }</small>
+    </div>
   </Card>
 )
