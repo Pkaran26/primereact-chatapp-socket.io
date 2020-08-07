7080364fce464dc9ba5ba3bbfab686aabbd89cde
Prateek Kaushik <pkaran26@Prateek>
2020-08-07 14:44:50 +0530

    minor

--- src/Components/Common/UserList.js
+++ src/Components/Common/UserList.js
@@ -1,23 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-const UserList = ({ onlineUsers, returnUser })=>{
-  const [users, setUsers] = useState([])
-
-  useEffect(()=>{
-    setUsers(onlineUsers)
-  }, [onlineUsers])
-
-  return (
-    <div className="userlist">
-      <ul>
-      { users && users.length>0?
-        users.map((e, i)=>(
-          <li className="user" onClick={ (el)=> returnUser(e) } key={ i }>{ e.first_name } { e.last_name }</li>
-        ))
-      :null }
-      </ul>
-    </div>
-  )
-}
+const UserList = ({ onlineUsers, returnUser })=>(
+  <div className="userlist">
+    <ul>
+    { onlineUsers && onlineUsers.length>0?
+      onlineUsers.map((e, i)=>(
+        <li className="user" onClick={ (el)=> returnUser(e) } key={ i }>
+          <span>{ e.first_name } { e.last_name }</span>
+        </li>
+      ))
+    :null }
+    </ul>
+  </div>
+)
 
 export default UserList
